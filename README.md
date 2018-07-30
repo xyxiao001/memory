ea1d92e9d451ace75235072c57ee5262a630efce
”xyxiao001” <463618622@qq.com>
2018-07-30 14:25:47 +0800

    login

--- README.md
+++
@@ -1,1 +0,0 @@
-###  react + react-router + ts + mobx + scss
--- src/view/User/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default class User extends React.Component {
-  public render() {
-    return (
-      <div>User</div>
-    );
-  }
-}
